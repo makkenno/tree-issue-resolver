e62ac529b42da0d09e7866822344c4b5af74036f
makkenno <kota.m.2774@gmail.com>
2024-11-10 22:55:17 +0900

    Devtoolsオフ

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -22,7 +22,7 @@ function RootComponent() {
       </div>
       <hr />
       <Outlet />
-      <TanStackRouterDevtools position="bottom-right" />
+      {/* <TanStackRouterDevtools position="bottom-right" /> */}
     </>
   );
 }
