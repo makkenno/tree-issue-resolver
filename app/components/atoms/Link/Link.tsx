03b58be550a5fdbb1d2694cd569136aac4753657
makkenno <kota.m.2774@gmail.com>
2024-12-27 02:36:43 +0900

    LinkコンポーネントのtextDecorationをとる

--- app/components/atoms/Link/Link.tsx
+++ app/components/atoms/Link/Link.tsx
@@ -1,3 +1,9 @@
 import { Link as RemixLink } from "@remix-run/react";
+import { RemixLinkProps } from "@remix-run/react/dist/components";
+import { FC } from "react";
 
-export const Link = RemixLink;
+export const Link: FC<
+  RemixLinkProps & React.RefAttributes<HTMLAnchorElement>
+> = (props) => (
+  <RemixLink {...props} style={{ textDecoration: "none" }}></RemixLink>
+);
