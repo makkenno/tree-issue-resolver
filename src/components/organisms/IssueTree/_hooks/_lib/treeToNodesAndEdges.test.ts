a93d980a04634fedded153b4e00d112d87bcf457
makkenno <kota.m.2774@gmail.com>
2024-12-26 22:10:39 +0900

    treeToNodesAndEdges修正

--- src/components/organisms/IssueTree/_hooks/_lib/treeToNodesAndEdges.test.ts
+++ src/components/organisms/IssueTree/_hooks/_lib/treeToNodesAndEdges.test.ts
@@ -3,17 +3,20 @@ import { describe, it, expect } from "vitest";
 
 describe("treeToNodesAndEdges", () => {
   it("1階層のツリーでちゃんと変換できること", () => {
-    const { nodes, edges } = treeToNodesAndeEdges({
-      id: "1",
-      title: "地球とは何か？",
-      isResolved: false,
-      children: [],
-    });
+    const { nodes, edges } = treeToNodesAndeEdges(
+      {
+        id: "1",
+        title: "地球とは何か？",
+        isResolved: false,
+        children: [],
+      },
+      { matches: false }
+    );
 
     const expectedNodes = [
       {
         id: "1",
-        position: { x: 0, y: 0 },
+        position: { x: 40, y: 40 },
         type: "issueCard",
         data: { id: "1", title: "地球とは何か？", isResolved: false },
       },
@@ -24,42 +27,45 @@ describe("treeToNodesAndEdges", () => {
   });
 
   it("2階層のツリーでちゃんと変換できること", () => {
-    const { nodes, edges } = treeToNodesAndeEdges({
-      id: "1",
-      title: "地球とは何か？",
-      isResolved: false,
-      children: [
-        {
-          id: "2",
-          title: "生物とは何か？",
-          isResolved: false,
-          children: [],
-        },
-        {
-          id: "3",
-          title: "宇宙とは何か？",
-          isResolved: true,
-          children: [],
-        },
-      ],
-    });
+    const { nodes, edges } = treeToNodesAndeEdges(
+      {
+        id: "1",
+        title: "地球とは何か？",
+        isResolved: false,
+        children: [
+          {
+            id: "2",
+            title: "生物とは何か？",
+            isResolved: false,
+            children: [],
+          },
+          {
+            id: "3",
+            title: "宇宙とは何か？",
+            isResolved: true,
+            children: [],
+          },
+        ],
+      },
+      { matches: false }
+    );
 
     const expectedNodes = [
       {
         id: "1",
-        position: { x: 0, y: 0 },
+        position: { x: 40, y: 40 },
         type: "issueCard",
         data: { id: "1", title: "地球とは何か？", isResolved: false },
       },
       {
         id: "2",
-        position: { x: 240, y: 0 },
+        position: { x: 312, y: 40 },
         type: "issueCard",
         data: { id: "2", title: "生物とは何か？", isResolved: false },
       },
       {
         id: "3",
-        position: { x: 240, y: 60 },
+        position: { x: 312, y: 140 },
         type: "issueCard",
         data: { id: "3", title: "宇宙とは何か？", isResolved: true },
       },
@@ -73,55 +79,58 @@ describe("treeToNodesAndEdges", () => {
   });
 
   it("3階層のツリーでちゃんと変換できること", () => {
-    const { nodes, edges } = treeToNodesAndeEdges({
-      id: "1",
-      title: "地球とは何か？",
-      isResolved: false,
-      children: [
-        {
-          id: "2",
-          title: "生物とは何か？",
-          isResolved: false,
-          children: [],
-        },
-        {
-          id: "3",
-          title: "宇宙とは何か？",
-          isResolved: true,
-          children: [
-            {
-              id: "4",
-              title: "銀河とは何か？",
-              isResolved: true,
-              children: [],
-            },
-          ],
-        },
-      ],
-    });
+    const { nodes, edges } = treeToNodesAndeEdges(
+      {
+        id: "1",
+        title: "地球とは何か？",
+        isResolved: false,
+        children: [
+          {
+            id: "2",
+            title: "生物とは何か？",
+            isResolved: false,
+            children: [],
+          },
+          {
+            id: "3",
+            title: "宇宙とは何か？",
+            isResolved: true,
+            children: [
+              {
+                id: "4",
+                title: "銀河とは何か？",
+                isResolved: true,
+                children: [],
+              },
+            ],
+          },
+        ],
+      },
+      { matches: true }
+    );
 
     const expectedNodes = [
       {
         id: "1",
-        position: { x: 0, y: 0 },
+        position: { x: 40, y: 40 },
         type: "issueCard",
         data: { id: "1", title: "地球とは何か？", isResolved: false },
       },
       {
         id: "2",
-        position: { x: 240, y: 0 },
+        position: { x: 340, y: 40 },
         type: "issueCard",
         data: { id: "2", title: "生物とは何か？", isResolved: false },
       },
       {
         id: "3",
-        position: { x: 240, y: 60 },
+        position: { x: 340, y: 140 },
         type: "issueCard",
         data: { id: "3", title: "宇宙とは何か？", isResolved: true },
       },
       {
         id: "4",
-        position: { x: 480, y: 60 },
+        position: { x: 640, y: 140 },
         type: "issueCard",
         data: { id: "4", title: "銀河とは何か？", isResolved: true },
       },
