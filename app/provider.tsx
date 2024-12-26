5b9e7e266c7c3c6c5cdc64c622d35f2a9859881a
makkenno <kota.m.2774@gmail.com>
2024-12-27 02:47:51 +0900

    ダークテーマをデフォルトにした

--- app/provider.tsx
+++ app/provider.tsx
@@ -3,5 +3,7 @@ import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 
 export const Provider: FC<{ children: ReactNode }> = ({ children }) => (
-  <MantineProvider theme={theme}>{children}</MantineProvider>
+  <MantineProvider theme={theme} defaultColorScheme="dark">
+    {children}
+  </MantineProvider>
 );
