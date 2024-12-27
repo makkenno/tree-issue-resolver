f25befb9e79f290dbd17d3cf929a03c3244735a8
makkenno <kota.m.2774@gmail.com>
2024-12-27 19:41:18 +0900

    IssueId value objectを作成

---
+++ app/core/domain/value-object/IssueId.spec.ts
@@ -0,0 +1,38 @@
+import { describe, expect, test } from "vitest";
+import { IssueId } from "./IssueId";
+
+describe("IssueId", () => {
+  test("空文字を入れて生成したらエラーになること", () => {
+    expect(() => IssueId.toString("")).toThrow();
+  });
+
+  test("uuid以外を入れて生成したらエラーになること", () => {
+    expect(() => IssueId.toString("hogehoge")).toThrow();
+  });
+
+  test("uuidを入れて生成したら生成できること", () => {
+    const rawUUID = "b9576b6b-45c0-440f-8703-5571466eef60";
+    expect(IssueId.toString(rawUUID).value).toBe(rawUUID);
+  });
+
+  test("オブジェクトを生成したらvalueにuuidが生成できること", () => {
+    expect(IssueId.create().value).toMatch(
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
+    );
+  });
+
+  test("同じuuidから生成したオブジェクトは等価", () => {
+    const rawUUID = "b9576b6b-45c0-440f-8703-5571466eef60";
+    const issueId1 = IssueId.toString(rawUUID);
+    const issueId2 = IssueId.toString(rawUUID);
+    expect(issueId1.isEqual(issueId2)).toBe(true);
+  });
+
+  test("違うuuidから生成したオブジェクトは等価", () => {
+    const rawUUID = "b9576b6b-45c0-440f-8703-5571466eef60";
+    const rawUUID2 = "00006b6c-45c0-440f-8703-5571466eef60";
+    const issueId1 = IssueId.toString(rawUUID);
+    const issueId2 = IssueId.toString(rawUUID2);
+    expect(issueId1.isEqual(issueId2)).toBe(false);
+  });
+});
---
+++ app/core/domain/value-object/IssueId.ts
@@ -0,0 +1,22 @@
+import * as z from "zod";
+
+export class IssueId {
+  private constructor(public readonly value: string) {
+    if (!z.string().uuid().safeParse(value).success) {
+      throw new Error(`invalid IssueId ${value}`);
+    }
+  }
+
+  static toString(rawId: string) {
+    return new IssueId(rawId);
+  }
+
+  static create(): IssueId {
+    const id = crypto.randomUUID();
+    return new IssueId(id);
+  }
+
+  public isEqual(issueId: IssueId): boolean {
+    return this.value === issueId.value;
+  }
+}
